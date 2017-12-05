d177093a108631eb5a51f2d57681f08274457b59
Ben Glynn <ben.glynn@just-eat.com>
2017-12-05 17:08:11 +0000

    Loading screen matches app #2

--- ios/ViewController.swift
+++ ios/ViewController.swift
@@ -12,6 +12,8 @@ import RxSwift
 class ViewController: UIViewController {
     
     @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var mask: UIView!
+    @IBOutlet weak var progress: UIImageView!
     
     let disposeBag = DisposeBag()
 
@@ -19,27 +21,34 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         let stateSubscription = store.state
             .delay(RxTimeInterval(5), scheduler: MainScheduler.instance)
-            .subscribe(onNext: {
-            let viewController = currentView(forState: $0)
-            let rootView = viewController.view!
-            rootView.translatesAutoresizingMaskIntoConstraints = false
-            self.addChildViewController(viewController)
-            self.containerView.addSubview(rootView)
-            NSLayoutConstraint.activate([
-                rootView.leadingAnchor.constraint(equalTo: self.containerView.leadingAnchor),
-                rootView.trailingAnchor.constraint(equalTo: self.containerView.trailingAnchor),
-                rootView.topAnchor.constraint(equalTo: self.containerView.topAnchor),
-                rootView.bottomAnchor.constraint(equalTo: self.containerView.bottomAnchor),
-            ])
-            viewController.didMove(toParentViewController: viewController)
-            
-        })
+            .take(1)
+            .subscribe(onNext: self.onState)
         stateSubscription.disposed(by: disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func onState(state: State) {
+        let viewController = currentView(forState: state)
+        let rootView = viewController.view!
+        rootView.translatesAutoresizingMaskIntoConstraints = false
+        self.addChildViewController(viewController)
+        self.containerView.addSubview(rootView)
+        self.matchAnchors(view1: rootView, view2: self.containerView)
+        viewController.didMove(toParentViewController: viewController)
+        self.mask.isHidden = true
+    }
+    
+    func matchAnchors(view1: UIView, view2: UIView) {
+        NSLayoutConstraint.activate([
+            view1.leadingAnchor.constraint(equalTo: view2.leadingAnchor),
+            view1.trailingAnchor.constraint(equalTo: view2.trailingAnchor),
+            view1.topAnchor.constraint(equalTo: view2.topAnchor),
+            view1.bottomAnchor.constraint(equalTo: view2.bottomAnchor),
+        ])
     }
 
 
