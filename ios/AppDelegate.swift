fca94e1b4a58e2e3af5079d9ff547165b4b067fb
Ben Glynn <ben.glynn@just-eat.com>
2017-12-13 14:42:24 +0000

    Prevent flash of snapshot on launch after kill by masking with launch storyboard #5

--- ios/AppDelegate.swift
+++ ios/AppDelegate.swift
@@ -17,26 +17,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
+    func applicationWillResignActive(_ application: UIApplication) {}
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        withTopmost(self.window!.rootViewController!) {
+            let mask = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateInitialViewController()!
+            $0.present(mask, animated: false, completion: nil)
+        }
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        withTopmost(self.window!.rootViewController!) {
+            $0.dismiss(animated: false, completion: nil)
+        }
     }
 
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
+    func applicationDidBecomeActive(_ application: UIApplication) {}
 
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    func applicationWillTerminate(_ application: UIApplication) {}
+    
+    func withTopmost(_ viewController: UIViewController, _ callback: (_ viewController: UIViewController)->Void) {
+        let presented = viewController.presentedViewController
+        if(presented != nil) {
+            withTopmost(presented!, callback)
+        } else {
+            callback(viewController)
+        }
     }
 
 
