2276f20ad78d95a27931ce1c37a94e14ab36fd00
Ben Glynn <ben.glynn@just-eat.com>
2017-12-13 17:24:42 +0000

    Animated tab bar #4

--- ios/Slices/Core/HomeView/HomeViewController.swift
+++ ios/Slices/Core/HomeView/HomeViewController.swift
@@ -4,6 +4,8 @@ class HomeViewController: UIViewController {
 
     @IBOutlet weak var rays: RaysView!
     @IBOutlet weak var contents: UIStackView!
+    @IBOutlet weak var bar: UIView!
+    @IBOutlet weak var barBottom: NSLayoutConstraint!
     
     var firstRun = true
     
@@ -12,18 +14,20 @@ class HomeViewController: UIViewController {
         if(firstRun == true) {
             rays.isFirstViewOfSession = firstRun
             contents.alpha = 0
+            barBottom.constant = 49
         }
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         if(firstRun == true) {
-            DispatchQueue.global().asyncAfter(deadline: DispatchTime.now() + 0.8) {
-                DispatchQueue.main.async {
-                    UIView.animate(withDuration: 1.0) { [weak self] in
-                        self?.contents.alpha = 1.0
-                    }
-                }
+            UIView.animate(withDuration: 0.5) { [weak self] in
+                self?.barBottom.constant = 0
+                self?.view.layoutIfNeeded()
+            }
+            UIView.animate(withDuration: 0.5) { [weak self] in
+                self?.contents.alpha = 1.0
+                self?.view.layoutIfNeeded()
             }
         }
         firstRun = false
