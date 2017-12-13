19c7d2a5590c496d17320737164d80d4d0d5205a
Ben Glynn <ben.glynn@just-eat.com>
2017-12-13 16:07:04 +0000

    Home view contents and animation #4

--- ios/Slices/Core/HomeView/HomeViewController.swift
+++ ios/Slices/Core/HomeView/HomeViewController.swift
@@ -3,9 +3,29 @@ import UIKit
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var rays: RaysView!
+    @IBOutlet weak var contents: UIStackView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        rays.isFirstViewOfSession = true // TODO: from state, don't animate more than once per session
+    var firstRun = true
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if(firstRun == true) {
+            rays.isFirstViewOfSession = firstRun
+            contents.alpha = 0
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if(firstRun == true) {
+            DispatchQueue.global().asyncAfter(deadline: DispatchTime.now() + 0.8) {
+                DispatchQueue.main.async {
+                    UIView.animate(withDuration: 1.0) { [weak self] in
+                        self?.contents.alpha = 1.0
+                    }
+                }
+            }
+        }
+        firstRun = false
     }
 }
