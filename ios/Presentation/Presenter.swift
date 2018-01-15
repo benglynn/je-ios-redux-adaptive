d6f7c421238557280beb7df0c3c0a660924c7905
Ben Glynn <ben.glynn@just-eat.com>
2018-01-15 16:56:56 +0000

    Completed presentation implementation with animation #26

--- ios/Presentation/Presenter.swift
+++ ios/Presentation/Presenter.swift
@@ -4,32 +4,27 @@ import UIKit
 struct Presenter {
     
     static func present(_ state: State, on parent: UIViewController, injecting store: Store) {
-        present(currentScreenFamilyStack(state), on: parent, injecting: store)
+        present(currentScreenFamilyStack(state), on: parent, isChild: false, injecting: store)
     }
     
-    private static func currentScreenFamilyStack(_ state: State) -> [ScreenFamily] {
-        let match = state.config.routes.first {
-            state.core.path.range(of: $0.pathPattern.rawValue, options: .regularExpression) != nil
-        }
-        print(match == nil ? "No path match" : "Path matched: \(match!.pathPattern)")
-        return match?.screens ?? state.config.routes[0].screens
-    }
-    
-    private static func present(_ screenFamilyStack: [ScreenFamily], on parent: UIViewController, injecting store: Store) {
-        let lastScreen = screenFamilyStack.count == 1,
-            family = screenFamilyStack[0],
-            screenType = family.screen.viewControllerType
+    private static func present(_ screenFamilyStack: [ScreenFamily], on parent: UIViewController, isChild: Bool, injecting store: Store) {
+        let family = screenFamilyStack[0],
+            screenType = family.screen.viewControllerType,
+            nextIsChild = family.children != nil,
+            nextIsLast = screenFamilyStack.count == 2,
+            thisIsLast = screenFamilyStack.count == 0,
+            isAnimated = (nextIsChild && nextIsLast) || thisIsLast
         
         let foundInParent: UIViewController? = {
             switch parent {
             case let tabs as UITabBarController:
                 if let matchingChild = (tabs.viewControllers?.first { type(of: $0) == screenType }) {
-                    print("Found \(family.screen) in \(parent)")
+//                    print("Found \(family.screen) in \(parent)")
                     return matchingChild
                 } else { return nil }
             case let navStack as UINavigationController:
                 if let matchingChild = (navStack.viewControllers.first { type(of: $0) == screenType }) {
-                    print("Found \(family.screen) in \(parent)")
+//                    print("Found \(family.screen) in \(parent)")
                     return matchingChild
                 } else { return nil }
             default: return nil
@@ -40,44 +35,59 @@ struct Presenter {
             if foundInParent != nil { return nil }
             
             if parent.presentedViewController != nil && type(of: parent.presentedViewController!) == screenType {
-                print("\(family.screen) already presented on \(parent)")
+//                print("\(family.screen) already presented on \(parent)")
                 return parent.presentedViewController
             } else {
-                print("Presenting \(family.screen) on \(parent)")
+//                print("Presenting \(family.screen) on \(parent)")
                 let viewController = family.screen.create(injecting: store)
-                parent.present(viewController, animated: lastScreen, completion: nil) // TODO: animate true for all but first
+                parent.present(viewController, animated: isAnimated, completion: nil) // TODO: animate true for all but first
                 return viewController
             }
         }()
         
         let foundOrPresented = foundInParent ?? presentedOnParent!
         
-        func reuseEach(_ incumbents: [UIViewController]?, orCreate screens: [Screen]) -> [UIViewController] {
-            guard incumbents != nil else { return screens.map { $0.create(injecting: store) } }
-            return zip(screens, incumbents!).map { screen, incumbent in
-                screen.viewControllerType == type(of: incumbent) ? incumbent : screen.create(injecting: store) }
-        }
-        
         if let childNames = family.children {
             guard screenFamilyStack.count > 0, childNames.contains(screenFamilyStack[1].screen) else {
                 fatalError("Expected next in stack to be child of \(foundOrPresented)")
             }
+            func reuseEach(_ incumbents: [UIViewController]?, orCreate screens: [Screen]) -> [UIViewController] {
+                guard incumbents != nil else { return screens.map { $0.create(injecting: store) } }
+                return screens.enumerated().map { (i, screen) in
+                    incumbents!.indices.contains(i) && type(of: incumbents![i]) == screen.viewControllerType
+                        ? incumbents![i] : screen.create(injecting: store)
+                }
+            }
+            let selectedChild = screenFamilyStack[1].screen
             switch foundOrPresented {
+                
             case let tabs as UITabBarController:
-                tabs.setViewControllers(reuseEach(tabs.viewControllers, orCreate: childNames), animated: true)
-                let selectedChild = screenFamilyStack[1].screen
-                print("Selecting \(selectedChild) in \(parent)")
+                tabs.setViewControllers(reuseEach(tabs.viewControllers, orCreate: childNames), animated: isAnimated)
+//                print("Selecting \(selectedChild) in \(parent)")
                 tabs.selectedViewController = tabs.viewControllers!.first { type(of: $0) == selectedChild.viewControllerType }
+                
             case let navStack as UINavigationController:
-                navStack.setViewControllers(childNames.map { $0.create(injecting: store) }, animated: false) // TODO: animated sometimes
+                guard childNames.last == selectedChild else {
+                    fatalError("Expected last child of \(parent) to be \(selectedChild)")
+                }
+//                print("Selecting \(selectedChild) in \(parent)")
+                navStack.setViewControllers(reuseEach(navStack.viewControllers, orCreate: childNames), animated: isAnimated)
                 
             default:
                 fatalError("Unable to add children to an unknown parent type")
             }
         }
-        if screenFamilyStack.count > 1 {
-            present(Array(screenFamilyStack[1...]), on: foundOrPresented, injecting: store)
+        if screenFamilyStack.count > 1 && !(nextIsLast && nextIsChild) {
+            present(Array(screenFamilyStack[1...]), on: foundOrPresented, isChild: nextIsChild, injecting: store)
         }
+    }
+    
+    private static func currentScreenFamilyStack(_ state: State) -> [ScreenFamily] {
+        let match = state.config.routes.first {
+            state.core.path.range(of: $0.pathPattern.rawValue, options: .regularExpression) != nil
+        }
+        print(match == nil ? "No path match" : "Path matched: \(match!.pathPattern)")
+        return match?.screens ?? state.config.routes[0].screens
     }
 }
 
