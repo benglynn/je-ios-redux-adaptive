ca7a61423321c3b428ea7b10640d08e4c0bca63a
Ben Glynn <ben.glynn@just-eat.com>
2018-01-24 07:03:22 +0000

    Removed mask from backgrounded app

--- ios/AppDelegate.swift
+++ ios/AppDelegate.swift
@@ -35,28 +35,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             
     }
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        return true
-    }
-
-    func applicationWillResignActive(_ application: UIApplication) {}
-
     func applicationDidEnterBackground(_ application: UIApplication) {
-        withTopmost(self.window!.rootViewController!) {
-            let mask = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateInitialViewController()!
-            $0.present(mask, animated: false, completion: nil)
-        }
+//        withTopmost(self.window!.rootViewController!) {
+//            let mask = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateInitialViewController()!
+//            $0.present(mask, animated: false, completion: nil)
+//        }
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        withTopmost(self.window!.rootViewController!) {
-            $0.dismiss(animated: false, completion: nil)
-        }
+//        withTopmost(self.window!.rootViewController!) { $0.dismiss(animated: false, completion: nil) }
     }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {}
-
-    func applicationWillTerminate(_ application: UIApplication) {}
     
     func withTopmost(_ viewController: UIViewController, _ callback: (_ viewController: UIViewController)->Void) {
         let presented = viewController.presentedViewController
