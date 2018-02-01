06a08c7d3f024948879c8a811c3e46fd85db1ff6
Ben Glynn <ben.glynn@just-eat.com>
2018-02-01 16:49:33 +0000

    Updated regex #44

--- ios/Presentation/PathPattern.swift
+++ ios/Presentation/PathPattern.swift
@@ -4,7 +4,7 @@ enum PathPattern: String {
     case HomePath = "^$"
     case OrdersPath = "^orders$"
     case SettingsPath = "^settings$"
-    case AreaPath = "^bs14dj$" // TODO: postcode regex
+    case AreaPath = "^[a-z]{1,2}[0-9][0-9a-z]?[0-9][a-z]{2}$" // postcode
 }
 
 typealias Route = (pathPattern: PathPattern, screens: [ScreenFamily])
