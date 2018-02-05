fd51a38f9b20854000f2a9e470dfe75e368986d1
Ben Glynn <ben.glynn@just-eat.com>
2018-02-05 14:03:28 +0000

    Menu settings no longer duplicates Settings #49

--- ios/Adaptations/Menu/Screens/MenuSettings/MenuSettingsViewController.swift
+++ ios/Adaptations/Menu/Screens/MenuSettings/MenuSettingsViewController.swift
@@ -6,7 +6,9 @@ class MenuSettingsViewController: UIViewController, Presentable {
     static var storyboardName = "MenuSettings"
     var store: Store!
     
-    // MARK: - IB
+    // MARK: - Interface Builder
+    
+    @IBOutlet weak var containerView: UIView!
     
     @IBAction func tapClose(_ sender: Any) {
         store.dispatch(DismissLastScreenFamilyAction())
@@ -15,5 +17,19 @@ class MenuSettingsViewController: UIViewController, Presentable {
     @IBAction func tapReset(_ sender: Any) {
         store.dispatch(PresentResetScreenAction())
     }
+    
+    // MARK: - Lifecycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let settingsView = UIStoryboard(name: "Settings", bundle: nil).instantiateInitialViewController() as! SettingsViewController
+        settingsView.setStore(store: store)
+        addChildViewController(settingsView)
+        settingsView.view.frame = containerView.frame
+        settingsView.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        containerView.addSubview(settingsView.view)
+        settingsView.didMove(toParentViewController: self)
+    }
+    
 }
 
