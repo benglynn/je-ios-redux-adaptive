141c401d603fd3dd08d893a129ec292f8526fc82
Ben Glynn <ben.glynn@just-eat.com>
2018-02-26 14:32:57 +0000

    Parsing all required data from search response #11

---
+++ ios/Slices/Search/Restaurant.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+struct Restaurant: Codable {
+    let cuisines: String
+    let deliveryCost: Decimal
+    let deliveryStartTime: String
+    let drivingDistance: Double
+    let isCollectNow: Bool
+    let isDeliveryNow: Bool
+    let isOpen: Bool
+    let isSponsored: Bool
+    let logUrl: String
+    let openingTime: String
+    let percentOff: Decimal
+    let rating: Double
+    let ratings: Int32
+    let title: String
+    
+    init(serviceRestaurant: SearchServiceResponse.Restaurant, isOpen: Bool) {
+        self.cuisines = serviceRestaurant.Cuisines.map { cuisine in cuisine.Name }.joined(separator: ", ")
+        self.deliveryCost = serviceRestaurant.DeliveryCost
+        self.deliveryStartTime = serviceRestaurant.DeliveryStartTime
+        self.drivingDistance = serviceRestaurant.DriveDistance
+        self.isCollectNow = serviceRestaurant.IsCollection
+        self.isDeliveryNow = serviceRestaurant.IsDelivery
+        self.isOpen = isOpen
+        self.isSponsored = serviceRestaurant.IsSponsored
+        self.logUrl = serviceRestaurant.LogoUrl
+        self.openingTime = serviceRestaurant.OpeningTime
+        self.percentOff = serviceRestaurant.OfferPercent
+        self.rating = serviceRestaurant.RatingDetails.StarRating
+        self.ratings = serviceRestaurant.RatingDetails.Count
+        self.title = serviceRestaurant.Name
+    }
+    
+}
--- ios/Slices/Search/SearchServiceResponse.swift
+++ ios/Slices/Search/SearchServiceResponse.swift
@@ -2,8 +2,30 @@ import Foundation
 
 struct SearchServiceResponse: Codable {
     
+    struct Cuisine: Codable {
+        let Name: String
+        let SeoName: String
+    }
+    
+    struct RatingDetailsStruct: Codable {
+        let Count: Int32
+        let StarRating: Double
+    }
+    
     struct Restaurant: Codable {
         let Name: String
+        let Cuisines: [Cuisine]
+        let DeliveryCost: Decimal
+        let DeliveryStartTime: String
+        let DriveDistance: Double
+        let IsCollection: Bool
+        let IsDelivery: Bool
+        let IsSponsored: Bool
+        let LogoUrl: String
+        let OfferPercent: Decimal
+        let OpeningTime: String
+        let RatingDetails: RatingDetailsStruct
+        
     }
     
     struct MetaDataStruct: Codable {
@@ -12,5 +34,5 @@ struct SearchServiceResponse: Codable {
     
     let OpenRestaurants: [Restaurant]
     let ClosedRestaurants: [Restaurant]
-    let MetaData: SearchServiceResponse.MetaDataStruct
+    let MetaData: MetaDataStruct
 }
