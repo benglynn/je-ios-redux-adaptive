d1c39c15a64b72311c09f0c7e4378d6a84bb96c9
Ben Glynn <ben.glynn@just-eat.com>
2018-03-06 11:38:55 +0000

    Cell details #11

--- ios/Slices/Search/Screens/Area/ResultCollectionViewCell.swift
+++ ios/Slices/Search/Screens/Area/ResultCollectionViewCell.swift
@@ -3,9 +3,23 @@ import UIKit
 class ResultCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var cuisines: UILabel!
+    @IBOutlet weak var distance: UILabel!
+    @IBOutlet weak var collectNow: UILabel!
+    @IBOutlet weak var deliveryCost: UILabel!
+    @IBOutlet weak var deliveryTime: UILabel!
+    @IBOutlet weak var preOrder: UILabel!
     
     func update(_ restaurant: Restaurant) {
         title.text = restaurant.title
+        cuisines.text = restaurant.cuisines
+        distance.text = "\(restaurant.drivingDistance) mi"
+        collectNow.isHidden = !(restaurant.isCollectNow && !restaurant.isDeliveryNow)
+        deliveryCost.isHidden = !restaurant.isDeliveryNow
+        deliveryTime.isHidden = !(!restaurant.isCollectNow && !restaurant.isDeliveryNow)
+        preOrder.isHidden = restaurant.isOpen
+        deliveryTime.text = "Delivering from \(restaurant.deliveryStartTime)"
+        deliveryCost.text = "Delivery £\(restaurant.deliveryCost)"
     }
 
 }
