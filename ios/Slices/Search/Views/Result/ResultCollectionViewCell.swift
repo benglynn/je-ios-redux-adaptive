5ee4a6dccbe6e40d9a1141e5d0b3f8edcabfb505
Ben Glynn <ben.glynn@just-eat.com>
2018-03-20 13:55:49 +0000

    Restructured directory #11

--- ios/Slices/Search/Views/Result/ResultCollectionViewCell.swift
+++ ios/Slices/Search/Views/Result/ResultCollectionViewCell.swift
@@ -68,8 +68,6 @@ class ResultCollectionViewCell: UICollectionViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-//        layer.borderColor = UIColor(red: 202/255, green: 202/255, blue: 202/255, alpha: 1.0).cgColor
-//        layer.borderWidth = 1.0
         layer.cornerRadius = 2.0
     }
 
